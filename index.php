0f65c842a0a6d161c84ec8da31d166273c0cc557
Imanuel Harkespan <harkespan@dsn.dinus.ac.id>
2024-09-04 11:31:50 +0700

    create dynamic route

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
 			require_once __DIR__ . '/vendor/autoload.php';
 			require_once 'routes.php';
-
+			
 			$url = isset($_GET['url']) ? rtrim($_GET['url'], '/') : '';
-			if (array_key_exists($url, $routes)) {
+			if (array_key_exists($url, $routes)) 
+			{
 				$controllerAction = explode('@', $routes[$url]);
 				$controller = $controllerAction[0];
 				$action = $controllerAction[1];
 
-				require_once "app/Classes/$controller.php";
-				$controller = new $controller;
-				$controller->$action();
+				$dynamicClassName = 'Classes\\' . $controller;
+				if (class_exists($dynamicClassName)) 
+				{
+					$instance = new $dynamicClassName();
+					$instance->$action();
+				} else {
+					echo "Class $dynamicClassName does not exist.";
+				}
+				
 			} else {
 				echo "404 - Page Not Found";
 			}
